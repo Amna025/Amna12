455c9e913fcf3f71624f1038967d13424aed4475
Amna025 <163379811+Amna025@users.noreply.github.com>
2024-03-23 03:28:49 +0500

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,6 @@
+function update(){
+    console.log("Updating results");
+}
+var input=document.getElementById('myinput');
+var outpu=document.getElementById('output');
+output.innerHTML=input.value;
